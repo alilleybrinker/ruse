422efa8be152a127ff9a53c85ee935937888977d
Andrew Brinker <me@andrewbrinker.com>
2016-06-11 14:45:57 -0700

    Renamed files

--- src/ruse/parse/mod.rs
+++ src/ruse/parse/mod.rs
@@ -2,10 +2,10 @@ pub mod error;
 pub mod lex;
 pub mod parse;
 pub mod syntax_tree;
-pub mod tokens;
+pub mod token;
 
 pub use self::error::*;
 pub use self::lex::*;
 pub use self::parse::*;
 pub use self::syntax_tree::*;
-pub use self::tokens::*;
+pub use self::token::*;
