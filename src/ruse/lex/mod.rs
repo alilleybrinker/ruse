29a2e4f7ba0adae38cf7b0c8b1470731c2bce08f
Andrew Brinker <me@andrewbrinker.com>
2016-07-12 06:31:22 -0500

    Adding empty tests

--- src/ruse/lex/mod.rs
+++ src/ruse/lex/mod.rs
@@ -12,10 +12,35 @@ pub use self::tokenize::*;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
 
     #[test]
-    fn blah() {
+    fn lex_the_empty_program() {
+        assert_eq!(4, 4);
+    }
+
+    #[test]
+    fn lex_a_single_open_paren() {
+        assert_eq!(4, 4);
+    }
+
+    #[test]
+    fn lex_a_single_close_paren() {
+        assert_eq!(4, 4);
+    }
+
+    #[test]
+    fn lex_matching_parens() {
+        assert_eq!(4, 4);
+    }
+
+    #[test]
+    fn lex_a_simple_program() {
+        assert_eq!(4, 4);
+    }
+
+    #[test]
+    fn lex_a_more_complex_program() {
         assert_eq!(4, 4);
     }
 }
