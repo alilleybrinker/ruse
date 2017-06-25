76d1c82b12b9f6d1744c4b5b94e481d58480ade3
刘祺 <3442853561@qq.com>
2017-06-25 23:07:07 +0800

    T

--- src/ruse/read/parse/mod.rs
+++ src/ruse/read/parse/mod.rs
@@ -1,4 +1,5 @@
 //! Generate a syntax tree from an input stream.
+//! 从输入流生成语法树(AST)。
 
 pub mod error;
 pub mod expr;
@@ -7,6 +8,7 @@ use read::lex::token::Token;
 use read::parse::error::Result;
 
 /// Parse an input string, returning a Ruse expression that can be evaluated.
+/// 解析输入字符串，返回可评估的 Ruse 表达式。
 pub trait Parse {
     /// Parse a type into an AST.
     fn parse(&self) -> Result;
@@ -14,6 +16,7 @@ pub trait Parse {
 
 impl Parse for Vec<Token> {
     /// Parse a vector of tokens into an AST.
+    /// 将标记序列解析为 AST。
     fn parse(&self) -> Result {
         unimplemented!()
     }
