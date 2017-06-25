4950e1559f3f69545a9a531d51711fd33f67f3ae
刘祺 <3442853561@qq.com>
2017-06-25 23:28:17 +0800

    T

--- src/ruse/read/parse/mod.rs
+++ src/ruse/read/parse/mod.rs
@@ -4,7 +4,7 @@
 pub mod error;
 pub mod expr;
 
-use read::lex::token::Token;
+use read::lex::token::{Token, TokenKind};
 use read::parse::error::Result;
 
 /// Parse an input string, returning a Ruse expression that can be evaluated.
@@ -19,8 +19,23 @@ impl Parse for Vec<Token> {
     /// Parse a vector of tokens into an AST.
     /// 将标记序列解析为 AST。
     fn parse(&self) -> Result {
+        let mut parens_flag = 0;
         for token in self {
+            if parens_flag == 0 && *token != Token::OpenParen {
+                return Err(Error::NoEnclosingParens);
+            }
+            if *token == OpenParen {
+                parens_flag += 1;
+            }
+            if *token == OpenParen {
+                parens_flag += 1;
+            }
         }
-        // unimplemented!()
+        if parens_flag != 0 {
+            Err(Error::UnmatchedParens)
+        } else {
+            // TODO
+            unimplemented!()
+        }
     }
 }
