246953bbad939010d43866889ab5b3da8296b5b3
Andrew Brinker <me@andrewbrinker.com>
2017-07-02 09:12:04 -0700

    Getting the parser set up

--- libruse-read/src/parse/mod.rs
+++ libruse-read/src/parse/mod.rs
@@ -6,57 +6,98 @@ pub mod expr;
 pub use parse::error::{Error, Result};
 use lex::token::Token;
 use parse::expr::Expr;
+use std::slice::Iter;
 
 /// Build an AST from a stream of tokens.
 pub fn parse<V: AsRef<[Token]>>(v: V) -> Result {
-    parse_expr(v.as_ref())
+    let v = v.as_ref();
+    let i = v.iter();
+    parse_expr(&i)
 }
 
 /// Parses a Ruse expression.
-fn parse_expr(v: &[Token]) -> Result {
+fn parse_expr(v: &Iter<Token>) -> Result {
     if let Ok(a) = parse_atom(v) {
-        Ok(a)
-    } else if let Ok(a) = parse_integer(v) {
-        Ok(a)
-    } else if let Ok(a) = parse_float(v) {
-        Ok(a)
-    } else if let Ok(a) = parse_string(v) {
-        Ok(a)
-    } else if let Ok(a) = parse_bool(v) {
-        Ok(a)
-    } else if let Ok(a) = parse_list(v) {
-        Ok(a)
-    } else {
-        Err(Error::InvalidProgram)
+        return Ok(a);
     }
+
+    if let Ok(a) = parse_integer(v) {
+        return Ok(a);
+    }
+
+    if let Ok(a) = parse_float(v) {
+        return Ok(a);
+    }
+
+    if let Ok(a) = parse_string(v) {
+        return Ok(a);
+    }
+
+    if let Ok(a) = parse_bool(v) {
+        return Ok(a);
+    }
+
+    if let Ok(a) = parse_list(v) {
+        return Ok(a);
+    }
+
+    Err(Error::InvalidProgram)
 }
 
 /// Parses a Ruse atom
-fn parse_atom(_v: &[Token]) -> Result {
+fn parse_atom(_v: &Iter<Token>) -> Result {
+    // Check if the next token is an atom. If it is, succeed.
+    // Otherwise, error out.
+    //
+    // Make sure to only have the iterator progress if the next
+    // token is actually an atom.
     unimplemented!()
 }
 
 /// Parses a Ruse integer
-fn parse_integer(_v: &[Token]) -> Result {
+fn parse_integer(_v: &Iter<Token>) -> Result {
+    // Check if the next token is an integer. If it is, succeed.
+    // Otherwise, error out.
+    //
+    // Make sure to only have the iterator progress if the next
+    // token is actually an integer.
     unimplemented!()
 }
 
 /// Parses a Ruse float
-fn parse_float(_v: &[Token]) -> Result {
+fn parse_float(_v: &Iter<Token>) -> Result {
+    // Check if the next token is a float. If it is, succeed.
+    // Otherwise, error out.
+    //
+    // Make sure to only have the iterator progress if the next
+    // token is actually a float.
     unimplemented!()
 }
 
 /// Parses a Ruse string
-fn parse_string(_v: &[Token]) -> Result {
+fn parse_string(_v: &Iter<Token>) -> Result {
+    // Check if the next token is a string. If it is, succeed.
+    // Otherwise, error out.
+    //
+    // Make sure to only have the iterator progress if the next
+    // token is actually a string.
     unimplemented!()
 }
 
 /// Parses a Ruse bool
-fn parse_bool(_v: &[Token]) -> Result {
+fn parse_bool(_v: &Iter<Token>) -> Result {
+    // Check if the next token is a bool. If it is, succeed.
+    // Otherwise, error out.
+    //
+    // Make sure to only have the iterator progress if the next
+    // token is actually a bool.
     unimplemented!()
 }
 
 /// Parses a Ruse list
-fn parse_list(_v: &[Token]) -> Result {
+fn parse_list(_v: &Iter<Token>) -> Result {
+    // Parse an opening delimiter, then a series of Ruse expressions
+    // until you hit the matching closing delimiter. If you hit
+    // a non-matching closing delimiter first, error out.
     unimplemented!()
 }
