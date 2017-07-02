de664c709a73dad6d11836b7887babe7756dec13
Andrew Brinker <me@andrewbrinker.com>
2017-07-02 08:55:40 -0700

    Aligned with Expr

--- libruse-read/src/parse/mod.rs
+++ libruse-read/src/parse/mod.rs
@@ -16,11 +16,13 @@ pub fn parse<V: AsRef<[Token]>>(v: V) -> Result {
 fn parse_expr(v: &[Token]) -> Result {
     if let Ok(a) = parse_atom(v) {
         Ok(a)
+    } else if let Ok(a) = parse_integer(v) {
+        Ok(a)
+    } else if let Ok(a) = parse_float(v) {
+        Ok(a)
     } else if let Ok(a) = parse_string(v) {
         Ok(a)
-    } else if let Ok(a) = parse_number(v) {
-        Ok(a)
-    } else if let Ok(a) = parse_quoted(v) {
+    } else if let Ok(a) = parse_bool(v) {
         Ok(a)
     } else if let Ok(a) = parse_list(v) {
         Ok(a)
@@ -29,22 +31,32 @@ fn parse_expr(v: &[Token]) -> Result {
     }
 }
 
+/// Parses a Ruse atom
 fn parse_atom(_v: &[Token]) -> Result {
     unimplemented!()
 }
 
+/// Parses a Ruse integer
+fn parse_integer(_v: &[Token]) -> Result {
+    unimplemented!()
+}
+
+/// Parses a Ruse float
+fn parse_float(_v: &[Token]) -> Result {
+    unimplemented!()
+}
+
+/// Parses a Ruse string
 fn parse_string(_v: &[Token]) -> Result {
     unimplemented!()
 }
 
-fn parse_number(_v: &[Token]) -> Result {
+/// Parses a Ruse bool
+fn parse_bool(_v: &[Token]) -> Result {
     unimplemented!()
 }
 
-fn parse_quoted(_v: &[Token]) -> Result {
-    unimplemented!()
-}
-
+/// Parses a Ruse list
 fn parse_list(_v: &[Token]) -> Result {
     unimplemented!()
 }
