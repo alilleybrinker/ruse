d9cedd662d4f98043c716e1a2bcc6f75beec050a
Andrew Brinker <me@andrewbrinker.com>
2017-07-02 08:53:27 -0700

    Doing parsing differently

--- libruse-read/src/parse/error.rs
+++ libruse-read/src/parse/error.rs
@@ -9,27 +9,22 @@ use std::result;
 /// A convenience type for returning either an `Expr` or a parsing error.
 pub type Result = result::Result<Expr, Error>;
 
+// TODO: Make these errors more useful.
 /// Indicates an error in parsing.
 #[derive(Debug, PartialEq)]
 pub enum Error {
-    /// Indicates that there were no outer parentheses.
-    NoEnclosingParens,
-    /// Indicates that the parentheses were not matched.
-    UnmatchedParens,
     /// Indicates that the program is empty.
     EmptyProgram,
-    /// Indicates an identifier was found in an unexpected position.
-    UnexpectedIdentifier(String),
+    /// Invalid program.
+    InvalidProgram,
 }
 
 impl error::Error for Error {
     /// A simple textual description of the error.
     fn description(&self) -> &str {
         match *self {
-            Error::NoEnclosingParens => "no enclosing parens",
-            Error::UnmatchedParens => "unmatched parens",
             Error::EmptyProgram => "empty program",
-            Error::UnexpectedIdentifier(..) => "unexpected identifier",
+            Error::InvalidProgram => "invalid program",
         }
     }
 }
@@ -37,9 +32,6 @@ impl error::Error for Error {
 impl fmt::Display for Error {
     /// More detailed information about the error.
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Error::UnexpectedIdentifier(ref s) => write!(f, "unexpected identifier '{}'", s),
-            _ => write!(f, "{}", (self as &error::Error).description()),
-        }
+        write!(f, "{}", (self as &error::Error).description())
     }
 }
--- libruse-read/src/parse/mod.rs
+++ libruse-read/src/parse/mod.rs
@@ -4,62 +4,47 @@ pub mod error;
 pub mod expr;
 
 pub use parse::error::{Error, Result};
-use lex::token::{Token, TokenKind};
+use lex::token::Token;
 use parse::expr::Expr;
-
-/// Struct to track the balance of parens
-struct Parens {
-    count: i64,
-}
-
-impl Parens {
-    /// Construct a new paren tracker
-    fn new() -> Self {
-        Parens { count: 0 }
-    }
-
-    /// Return whether parens are balanced
-    fn matching(&self) -> bool {
-        self.count != 0
-    }
-
-    /// Return whether parens are not balanced
-    fn not_matching(&self) -> bool {
-        !self.matching()
-    }
-}
 
 /// Build an AST from a stream of tokens.
 pub fn parse<V: AsRef<[Token]>>(v: V) -> Result {
-    let v = v.as_ref();
-    let ast: Option<Expr> = None;
-    let mut parens = Parens::new();
+    parse_expr(v.as_ref())
+}
 
-    for token in v {
-        if parens.matching() && !token.kind.is_open_delim() {
-            return Err(Error::NoEnclosingParens);
-        }
-
-        match token.kind {
-            TokenKind::OpenDelim(..) => parens.count += 1,
-            TokenKind::CloseDelim(..) => parens.count -= 1,
-            TokenKind::Ident(ref s) => {
-                if parens.matching() {
-                    return Err(Error::UnexpectedIdentifier(s.to_owned()));
-                }
-
-                // uhhh
-            }
-            _ => {}
-        }
+/// Parses a Ruse expression.
+fn parse_expr(v: &[Token]) -> Result {
+    if let Ok(a) = parse_atom(v) {
+        Ok(a)
+    } else if let Ok(a) = parse_string(v) {
+        Ok(a)
+    } else if let Ok(a) = parse_number(v) {
+        Ok(a)
+    } else if let Ok(a) = parse_quoted(v) {
+        Ok(a)
+    } else if let Ok(a) = parse_list(v) {
+        Ok(a)
+    } else {
+        Err(Error::InvalidProgram)
     }
+}
 
-    if parens.not_matching() {
-        return Err(Error::UnmatchedParens);
-    }
+fn parse_atom(_v: &[Token]) -> Result {
+    unimplemented!()
+}
 
-    match ast {
-        Some(a) => Ok(a),
-        None => Err(Error::EmptyProgram),
-    }
+fn parse_string(_v: &[Token]) -> Result {
+    unimplemented!()
+}
+
+fn parse_number(_v: &[Token]) -> Result {
+    unimplemented!()
+}
+
+fn parse_quoted(_v: &[Token]) -> Result {
+    unimplemented!()
+}
+
+fn parse_list(_v: &[Token]) -> Result {
+    unimplemented!()
 }
